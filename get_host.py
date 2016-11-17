f550a1aee7c5a948b9f2e017a72ce85d8fc3d4e3
Mircea Ilie Ploscaru <tzekid2010@gmail.com>
2016-11-17 08:12:39 +0100

    started refractoring

--- get_host.py
+++ get_host.py
@@ -7,11 +7,11 @@ import multiprocessing
 ##### get_hosts
 ### v0.3
 # GOALS:
-#   [ ] introduce async functions
-#   [ ] func: spawn a process inside a container
-#   [ ] fix the bug where processes don't write to their RESULT
+#   [ ] write some documentation
+#   [ ] refractor the v0.2 code
+#   [ ] introduce async threads
 #   [ ] keep track of all [DONE | TOTAL] IPs in each process
-#   [ ] fix the deadlock problem @ async processes
+#   [ ] fix the x race @ async threads when writing to file
 
 ### v0.2
 # GOALS:
@@ -31,6 +31,7 @@ print("BOP")
 # print('%-24s%-48s%-24s%-8s' % ("IP", "DNS", "DOMAIN", "RESPONSE"))
 print('%-24s%-64s%-48s%-8s' % ("IP", "DNS", "DOMAIN", "RESPONSE"))
 
+### GLOBAL functions
 def split_array(array, parts = 1):
     length = len(array)
     return [ array[i*length // parts: (i+1)*length // parts] 
@@ -41,43 +42,43 @@ def val_to_text(val):
     elif val == 0:  return "OK"
     elif val == 1:  return "UNCHANGED"
 
-# socket.getfqdn is just socket.gethostbyname, but returns the best dns as string
-def get_dns(ip):
-    if socket.getfqdn(ip) != ip: return socket.getfqdn(ip)
-    else:                        return "FAIL"
-
-###	This pattern detects links and breaks the down in regexp groups.
-# eg. from domain.co.uk
-# group(4) => `domain_name`
-# group(5) => `co`
-# group(9) => `.uk`
-pattern = re.compile("((www\.)|([a-z0-9_\-]{1,}\.)+)?([a-z0-9_\-]{3,})\.([a-z]{2,4})(\/([a-z0-9_\-]{1,}\/)+)?([a-z0-9_\-]{1,})?(\.[a-z]{2,})?(\?)?(((\&)?[a-z0-9_\-]{1,}(\=[a-z0-9_\-]{1,})?)+)?")
-def get_hostname(dns):
-    match = re.search(pattern, dns)
-    if match:
-        hostname = match.group(4) + "." + match.group(5)
-        # Add the second domain to hostname if the hostname has a double dot domain ending e.g. "domain.co.uk"
-        hostname = hostname + match.group(9) if str(match.group(9)) != 'None' else hostname
-        return hostname
-    else: return "FAIL"
-
-# I thought of testing the hostnames by 
-# pinging the hostname on port 587 ... what could go wrong ? 
-# hping requires _sudo_ rights to be run.
-def ping(hostname, hping=False):
-    if hping:
-        try: response = os.system("sudo hping3 --faster -c 1 -S -p 587 " + hostname + " > /dev/null 2>&1")
-        except: print("hping is not installed !"); sys.exit()
-    else: response = os.system("ping -c 1 -w2 " + hostname + " > /dev/null 2>&1")
-
-    if response == 0: return  0
-    else:             return -1
-
 class process:
     def __init__(self):
         self.IPZ = []
         self.RESULTS = {}
-        self.DONE = False
+
+    # socket.getfqdn is just socket.gethostbyname, but returns the best dns as string
+    def get_dns(ip):
+        if socket.getfqdn(ip) != ip: return socket.getfqdn(ip)
+        else:                        return "FAIL"
+
+    ###	This pattern detects links and breaks the down in regexp groups.
+    # eg. from domain.co.uk
+    # group(4) => `domain_name`
+    # group(5) => `co`
+    # group(9) => `.uk`
+    pattern = re.compile("((www\.)|([a-z0-9_\-]{1,}\.)+)?([a-z0-9_\-]{3,})\.([a-z]{2,4})(\/([a-z0-9_\-]{1,}\/)+)?([a-z0-9_\-]{1,})?(\.[a-z]{2,})?(\?)?(((\&)?[a-z0-9_\-]{1,}(\=[a-z0-9_\-]{1,})?)+)?")
+    def get_hostname(dns):
+        match = re.search(pattern, dns)
+        if match:
+            hostname = match.group(4) + "." + match.group(5)
+            # Add the second domain to hostname if the hostname has a double dot domain ending e.g. "domain.co.uk"
+            hostname = hostname + match.group(9) if str(match.group(9)) != 'None' else hostname
+            return hostname
+        else: return "FAIL"
+    
+    # I thought of testing the hostnames by 
+    # pinging the hostname on port 587 ... what could go wrong ? 
+    # hping requires _sudo_ rights to be run.
+    def ping(hostname, hping=False):
+        if hping:
+            try: response = os.system("sudo hping3 --faster -c 1 -S -p 587 " + hostname + " > /dev/null 2>&1")
+            except: print("hping is not installed !"); sys.exit()
+        else: response = os.system("ping -c 1 -w2 " + hostname + " > /dev/null 2>&1")
+
+        if response == 0: return  0
+        else:             return -1
+
 
     def start(self):
         for ip in self.IPZ:
@@ -95,15 +96,23 @@ class process:
             if response == "FAIL":      continue
             else:                       self.RESULTS[ip] = hostname
 
-    def write_results(self, filename):
-        for key in self.RESULTS.keys():
-            print(filename, key + " " + self.RESULTS[key])
-
 class container:
-    def __init__(self, process_limit = 10):
+    def __init__(self):
         self.ips_array = []
-        self.process_limit = process_limit
         self.PROCESSES = []
+
+    def get_ips_as_array(filename):
+        OUTPUT = []
+        try: File = open(filename, "r+")
+        except:
+            print(File + " does not exist.")
+            sys.exit()
+
+        with File as f:
+            for line in f:
+                line = line.strip() # strip ≜ trim
+                OUTPUT.append(line)
+        return OUTPUT
 
     # TODO(kid): don't allow instances of the same process
     def add_ips(self, filename):    self.ips_array = get_ips_as_array(filename)
@@ -117,54 +126,8 @@ class container:
         for p in self.PROCESSES:
             p.IPZ = arrays[i]
             i += 1
-
-    def start_process(self, p): p.start()
-        # for p in self.PROCESSES:
-        #     p.start()
-
-def get_ips_as_array(filename):
-    OUTPUT = []
-    try: File = open(filename, "r+")
-    except:
-        print(File + " does not exist.")
-        sys.exit()
-
-    with File as f:
-        for line in f:
-            line = line.strip() # strip ≜ trim
-            OUTPUT.append(line)
-    return OUTPUT
-
-def write_output(filename, string):
-    try: output = open(filename, "w+")
-    except:
-        print(filename + " does not exist.")
-        sys.exit()
-
-    ouput.write(string + "\n")
-    output.close()
-
-fl = "ipz"
-
-c = container()
-c.add_ips(fl)
-
-p1 = process()
-p2 = process()
-
-c.add_process(p1)
-c.add_process(p2)
-
-c.divide_ips()
-
-def write_shit(p):
-    print("output")
-
-if __name__ == "__main__":
-    p = multiprocessing.Pool(4)
-    p.map(c.start_process, c.PROCESSES)
     
-    p.map(write_shit, c.PROCESSES)
+    # TODO(kid): start processes according to the nr. of threads ?
 
 
 print("EOP")
--- test/00_threading.py
+++ test/00_threading.py
@@ -1,7 +1,27 @@
 #!/usr/bin/python3
 
-# def f(x): return x*x
-def f(x): print(x*x)
+###
+#   Tried both parallel processing and
+# multi-threading and I found that threads
+# are just plain easier to implement than processes.
+#
+#   In this example I just made an f function which,
+# like the start / main function, takes one argument 
+# and doesn't return anything. All I now have to do
+# is find a way (@get_host) on how to write the results
+# to a file.
+### 
+
+import time
+import random
+import sys, os
+# def f(x):
+#     time.sleep(random.random()*10)
+#     return x*x
+    
+def f(x):    
+    time.sleep(random.random())
+    print(x*x)
 
 import threading
 
@@ -20,17 +40,16 @@ while(True):
         x = threading.Thread(target=f, args=([i]), kwargs={})
         x.start()
         i += 1
-    else:
-        x.join()
 
     if not y.is_alive() and j <= 100:
         y = threading.Thread(target=f, args=([j]), kwargs={})
         y.start()
         j += 1
-    else:
-        y.join()
     
     if i >= 101 and j >= 101:
         # print(str(x.is_alive) +" and "+ str(y.is_alive))
         if x.is_alive and y.is_alive:
+            x.join()
+            y.join()
             break
+        
